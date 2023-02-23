decd73d0af395cd3825e240dc7f1478e0d79a47c
OUSSADI Karim <oussadikarim00@gmial.com>
2023-02-23 09:50:07 +0100

    get refreces of all models added

--- controllers/candidatureControllers.js
+++ controllers/candidatureControllers.js
@@ -1,101 +1,85 @@
-
-import {Candidature} from "../models/candidature.js" 
+import { Candidature } from "../models/candidature.js";
+import { Candidat } from "./../models/candidat.js";
+import { Offre } from "./../models/offre.js";
 
 //read condidatures :
-   
-export const readCandidature = async(req,res)=>{
-    try{
-        const candidature = await Candidature.find({});
-        res.send(candidature)
-         
-    }
-    catch(err){
-        res.send(err)
-    }
-}
+
+export const readCandidature = async (req, res) => {
+  try {
+    const candidature = await Candidature.find({});
+    res.send(candidature);
+  } catch (err) {
+    res.send(err);
+  }
+};
 
 //read one condidature :
 
-export const readOneCandidature = async(req,res)=>{
-    try{
-        const id = req.params.id;
-        const candidature = await Candidature.findById(id)    
-        res.send(candidature)
-    }
-    catch(err){
-        res.send(err)
-    }
-}
+export const readOneCandidature = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const candidature = await Candidature.findById(id);
+    res.send(candidature);
+  } catch (err) {
+    res.send(err);
+  }
+};
 
 //update one state condidature:
 
-export const updateStateCandidature = async(req,res)=>{
-      try{
-        const id = req.params.id;
-        const candidature = await Candidature.findById(id)
-        const state = req.body.state
+export const updateStateCandidature = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const candidature = await Candidature.findById(id);
+    const state = req.body.state;
 
-        if (state !== "enCours" && state !== "refus" && state !== "accepted") {
-            return res.json("state value not correct")
-        }else {
-            const updateStateCandidature = await candidature.updateOne({
-                state:state
-            })
-            res.send(updateStateCandidature)
-
-        }
-
-      }
-      catch(err){
-        res.send(err)
-      }
-}
-
+    if (state !== "enCours" && state !== "refus" && state !== "accepted") {
+      return res.json("state value not correct");
+    } else {
+      const updateStateCandidature = await candidature.updateOne({
+        state: state,
+      });
+      res.send(updateStateCandidature);
+    }
+  } catch (err) {
+    res.send(err);
+  }
+};
 
 export const newCandidature = async (req, res) => {
-    try {
-        const new_candidatutre = new Candidature({
-            candidat: req.body.candidat,
-            offre: req.body.offre,
-        });
+  const userId = req.cookies["userId"].userId;
+  const candidat = await Candidat.find({ userInherit: userId });
+  const candidatId = candidat[0]._id;
+  const offre = await Offre.find({ title: req.body.title });
+  const offreId = offre[0]._id;
+  try {
+    const new_candidatutre = new Candidature({
+      candidat: candidatId,
+      offre: offreId,
+    });
 
-        await new_candidatutre.save()
-        res.send(new_candidatutre)
-    }
-    catch (err) {
-        console.log(err)
-    }
-}
-
-
-
+    await new_candidatutre.save();
+    res.send(new_candidatutre);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 export const getCandidature = async (req, res) => {
-    try {
-        await Candidature.find({})
-            .then(result => {
-                res.send(result)
-            })
-
-    }
-
-    catch (err) {
-
-        console.log(err)
-
-    }
-}
-
+  try {
+    await Candidature.find({}).then((result) => {
+      res.send(result);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 export const deleteCandidature = async (req, res) => {
-    try {
-        await Candidature.findOneAndDelete({id:req.params.id})
-        res.send('candidatutre supprimer')
-    }
-    catch (err) {
-        console.log(err)
-    }
-}
-
-
-
+  try {
+    await Candidature.findOneAndDelete({ id: req.params.id });
+    res.send("candidatutre supprimer");
+  } catch (err) {
+    console.log(err);
+  }
+};
--- controllers/offreControllers.js
+++ controllers/offreControllers.js
@@ -1,113 +1,94 @@
-
-import {Offre} from "../models/offre.js"
-
+import { Offre } from "../models/offre.js";
+import { Categorie } from "./../models/categorie.js";
+import { Recruteur } from "./../models/recruteur.js";
 
 // create offre :
-export const createOffre = async(req,res)=>{
- //logic to get recruteur id? from user id
+export const createOffre = async (req, res) => {
+  //logic to get recruteur id? from user id
+  const userId = req.cookies["userId"].userId;
+  const categorieId = await Categorie.find({ nom: req.body.categories })._id;
+  const recruteurId = await Recruteur.find({
+    userInherit: userId,
+  });
+  console.log(userId);
+  console.log(recruteurId[0]._id);
+  console.log(categorieId);
+  const offre = new Offre({
+    recruteur: recruteurId[0]._id,
+    title: req.body.title,
+    description: req.body.description,
+    dateLimit: req.body.dateLimit,
+  });
+  offre.categories.push(categorieId);
 
-    const offre = new Offre({
-        // recruteur:req.body_id,
-        categories:req.body.categories,
-        title:req.body.title,
-        description:req.body.description,
-        dateLimit:req.body.dateLimit
-    })
-    
-    try {
-        await offre.save()
-        res.json(offre)
-        
-    } catch (error) {
-        res.json(error)
-    }
-
-
-}
+  try {
+    await offre.save();
+    res.json(offre);
+  } catch (error) {
+    res.json(error);
+  }
+};
 
 // show specific offre :
 
-export const showOffre = async(req,res)=>{
-    try{
-        const id = req.params.id;
-        const offre = await Offre.findById(id)
- 
-           res.send(offre)
-    
+export const showOffre = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const offre = await Offre.findById(id);
 
-    }
-    catch(err){
-        res.send(err)
-    }
-} 
+    res.send(offre);
+  } catch (err) {
+    res.send(err);
+  }
+};
 
 // update specific offre :
 
-export const updateOffre = async(req,res)=>{
-    try{
-        const id = req.params.id;
-        const offre = await Offre.findById(id)
-        const updatedOffre = await offre.updateOne(
-           { categories : req.body.categories,
-            title:req.body.title,
-            dateLimit: req.body.dateLimit,
-            description: req.body.description}
-        )
- 
-           res.send(offre)
-    
+export const updateOffre = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const offre = await Offre.findById(id);
+    const updatedOffre = await offre.updateOne({
+      categories: req.body.categories,
+      title: req.body.title,
+      dateLimit: req.body.dateLimit,
+      description: req.body.description,
+    });
 
-    }
-    catch(err){
-        res.send(err)
-    }
-} 
+    res.send(offre);
+  } catch (err) {
+    res.send(err);
+  }
+};
 
 // delete specific offre
 
-export const deleteOffre = async(req,res)=>{
-    try{
-        const id = req.params.id;
-        const offre = await Offre.findByIdAndDelete(id);
-       
-        
+export const deleteOffre = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const offre = await Offre.findByIdAndDelete(id);
 
-        res.send("is deleted")
-    
-
-    }
-    catch(err){
-        res.send(err)
-    }
-} 
-
-
-
+    res.send("is deleted");
+  } catch (err) {
+    res.send(err);
+  }
+};
 
 export const getOffres = async (req, res) => {
-    try {
-        await Offre.find({})
-            .then(result => {
-                res.send(result)
-            })
-
-    }
-
-    catch (err) {
-
-        console.log(err)
-
-    }
-}
+  try {
+    await Offre.find({}).then((result) => {
+      res.send(result);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 export const Oneoffre = async (req, res) => {
-    try {
-      const offre =  await Offre.findOne({id:req.params.id})
-        res.send(offre)
-    }
-    catch (err) {
-        console.log(err)
-    }
-}
-
-
+  try {
+    const offre = await Offre.findOne({ id: req.params.id });
+    res.send(offre);
+  } catch (err) {
+    console.log(err);
+  }
+};
